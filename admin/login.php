f05325e612fcd5900433803a0121196ac52a5c96
Walsh Fernandes <walshfernades.320@gmail.com>
2019-06-11 15:18:06 +0530

    added styles to login page.

--- admin/login.php
+++ admin/login.php
@@ -14,13 +14,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>EMS - Admin Login</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
-    <!-- Notifty.js -->
-    <script src="../notify.js"></script>
+    <?php include("../utils/stylesheets.php");?>
     <!--Custom styles-->
     <link rel="stylesheet" href="css/login.css">
 </head>
