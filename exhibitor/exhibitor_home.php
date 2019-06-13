c4a37d75a3b43fa9bae9cc32d9f3ccaf8b19f6e3
Walsh Fernandes <walshfernades.320@gmail.com>
2019-06-13 19:13:06 +0530

    made some options for the exhibitor menu.

---
+++ exhibitor/exhibitor_home.php
@@ -0,0 +1,23 @@
+<div>
+    <div class="home-header">
+        <h4>Welcome to Exhibitor Management System</h4>
+    </div>
+    <div class="home-content">
+        <p>
+            Dear Exhibitor,<br><br>
+            Thank you for participating in Intimasia, Delhi – one of its kind event which brings together Brands, Retailers and
+            Distributors – all under one roof. This Exhibitor’s Manual provides you with all the details of the exhibition and will
+            assist you in preparing for your participation in the Fair.
+            Please read through this manual carefully and observe all the deadlines specified on the order forms for Additional
+            facilities and services.
+            If you require further assistance please feel free to contact our offices.
+            Looking forward to welcoming you in DELHI soon.
+        </p>
+        For <strong>PEPPERMINT COMMUNICATIONS PVT. LTD.,</strong><br>
+        <img src="../images/organizer_signature.jpg" alt="organizer's signature">
+        <br>Yusuf Dohadwala<br>
+        Chief Organizer
+
+        
+    </div>
+</div>
--- exhibitor/index.php
+++ exhibitor/index.php
@@ -16,7 +16,7 @@
     <title>EMS - Admin Dashboard</title>
     <?php require('../utils/stylesheets.php'); ?>
     <link rel="stylesheet" href="css/index.css">
-    <link rel="stylesheet" href="css/add_exhibitor.css">
+    <link rel="stylesheet" href="css/exhibitor_home.css">
 </head>
 
 <?php require('../utils/header.php'); ?>
@@ -27,28 +27,47 @@
         <div class="row">
             <div class="col-lg-3">
                 <ul class="nav flex-column nav-pills side-tab" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                    <div class="dropdown-divider"></div>
+                    <div class="sidebar-header">Exhibitor Manual</div>
+                    <div class="dropdown-divider"></div>
+
                     <li class="nav-item">
-                        <a class="nav-link active" id="v-pills-tab1" data-toggle="pill" href="#v-pills-1" role="tab" aria-controls="v-pills-home" aria-selected="true"><i class="fa fa-pen"></i>Edit Exhibitor Info</a>
+                        <a class="nav-link active" id="v-pills-tab1" data-toggle="pill" href="#v-pills-1" role="tab" aria-controls="v-pills-home" aria-selected="true"><i class="fa fa-home"></i>Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="v-pills-tab1" data-toggle="pill" href="#v-pills-1" role="tab" aria-controls="v-pills-home" aria-selected="true"><i class="fas fa-exclamation-triangle"></i>General Rules & Regulations</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="v-pills-tab1" data-toggle="pill" href="#v-pills-1" role="tab" aria-controls="v-pills-home" aria-selected="true"><i class="far fa-id-badge"></i>Company Profile</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="v-pills-tab1" data-toggle="pill" href="#v-pills-1" role="tab" aria-controls="v-pills-home" aria-selected="true"><i class="fas fa-exclamation-triangle"></i>Key contacts</a>
                     </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-asterisk"></i>Mandatory Forms</a>
                         <div class="dropdown-menu">
                             <!-- create additional tabs in the tab content div and then use href and data-toggle=pill to make them visible using the pills-->
                             <a class="dropdown-item" data-toggle="pill" href="#" role="tab">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <a class="dropdown-item" href="#">Something else here</a>
+                            <a class="dropdown-item" data-toggle="pill" href="#" role="tab">Another action</a>
+                            <a class="dropdown-item" data-toggle="pill" href="#" role="tab">Something else here</a>
                         </div>
                     </li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><i class="fab fa-wpforms"></i>Mandatory Forms</a>
+                        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><i class="fab fa-wpforms"></i>Optional Services</a>
                         <div class="dropdown-menu">
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <a class="dropdown-item" href="#">Something else here</a>
+                            <a class="dropdown-item" data-toggle="pill" href="#" role="tab">Action</a>
+                            <a class="dropdown-item" data-toggle="pill" href="#" role="tab">Another action</a>
+                            <a class="dropdown-item" data-toggle="pill" href="#" role="tab">Something else here</a>
                         </div>
                     </li>
+                    <div class="dropdown-divider"></div>
+                    <div class="sidebar-header">Account Actions</div>
+                    <div class="dropdown-divider"></div>
                     <li class="nav-item">
-                        <a class="nav-link" id="v-pills-tab4" data-toggle="pill" href="#v-pills-4" role="tab" aria-controls="v-pills-settings" aria-selected="false"><i class="fa fa-key"></i>Change Exhibitor Password</a>
+                        <a class="nav-link" id="v-pills-tab4" data-toggle="pill" href="#v-pills-4" role="tab" aria-controls="v-pills-settings" aria-selected="false"><i class="fa fa-pen"></i>Edit Exhibitor info</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="v-pills-tab4" data-toggle="pill" href="#v-pills-5" role="tab" aria-controls="v-pills-settings" aria-selected="false"><i class="fa fa-key"></i>Change Exhibitor Password</a>
                     </li>
                 </ul>
             </div>
@@ -56,7 +75,7 @@
             <div class="col-lg-9">
                 <div class="tab-content" id="v-pills-tabContent">
                     <div class="tab-pane fade show active" id="v-pills-1" role="tabpanel" aria-labelledby="v-pills-home-tab">
-                        
+                       <?php include("exhibitor_home.php");?>  
                     </div>
                     <div class="tab-pane fade" id="v-pills-2" role="tabpanel" aria-labelledby="v-pills-profile-tab">
 
@@ -65,7 +84,13 @@
                         
                     </div>
                     <div class="tab-pane fade" id="v-pills-4" role="tabpanel" aria-labelledby="v-pills-settings-tab">
+                        
+                    </div>
+                    <div class="tab-pane fade" id="v-pills-5" role="tabpanel" aria-labelledby="v-pills-messages-tab">
                         <?php require("password_change.php"); ?>
+                    </div>
+                    <div class="tab-pane fade" id="v-pills-6" role="tabpanel" aria-labelledby="v-pills-messages-tab">
+                        
                     </div>
                 </div>
             </div>
