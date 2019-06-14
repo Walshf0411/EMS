af3347680486f4e80a137c4368ec9312e3418f0a
Rahul Nandrajog <2016.rahul.nandrajog@ves.ac.in>
2019-06-14 22:03:32 +0530

    form 9 added

---
+++ exhibitor/exhibitor_mandatory_form9.php
@@ -0,0 +1,60 @@
+<?php require('../utils/form_logo_details.php'); ?>
+
+    <div class="col-md-12 col-sm-12">
+        <p class="table">
+            <table style="width:100%;">
+                <tr style="background-color:rgb(193, 13, 109);">
+                    <th>FORM 9</th>
+                    <th>UNDERTAKING</th>
+                    <th>MANDATORY</th>
+                </tr>
+                <tr>
+                    <td colspan="3">Submission Date - 5th June 2019 </td>
+                </tr>
+            </table>
+        </p>
+
+    </div>
+    <p>
+        <label for="Comapny Name"><strong>CompanyName:</strong></label>
+        <input type="text" name="companyName" class="data-input" style="width:75%;" >
+    </p>        
+    <p id="booth">
+        <label for="Booth number">Booth number:</label>
+        <input name="booth_number">
+    </p>
+
+    <pre>
+To,
+The Chief Organiser
+PEPPERMINT COMMUNICATIONS PVT. LTD.
+Mumbai
+</pre>
+        <strong>Subject:</strong> Undertaking towards participation at Super Juniorz, Chennai 2019
+        <p>
+        We, hereby undertake not to leave our booth unattended on 24th & 25th June 2019 from 10AM to 7PM on both the days at
+        Super Juniorz 2019. Furthermore, we shall abide by all the rules and regulations as mentioned in participation form and Exhibitors
+        Manual. We also understand the dismantling of booths before the end of exhibition or leaving the booths unattended shall attract a
+        penalty of Rs.50,000 (Rupees Fifty Thousand Only) in addition to being bared from participating in any future event of Peppermint
+        Communications Pvt. Ltd.
+        </p>
+
+    <div style="float:right;">
+        <pre>
+        (PLEASE TYPE IN BLOCK LETTERS)
+        (OR ATTACH BUSINESS NAME CARD)</pre>
+        <input oninput="this.className = ''" name="business_name_card"class="data-input">
+        <p>Name,Signature & Stamp of an Authorized Person</p>
+        <input oninput="this.className = ''" name="business_name_card"class="data-input">
+        <p>Mobile Number</p>
+    </div>
+    <div style="float:left;" id="receiver">
+        <pre>
+    SEND THIS FORM TO :
+    <strong>PEPPERMINT COMMUNICATIONS PVT. LTD.</strong> 
+    Unit B-135, Antophill Warehousing Complex, V.I.T. College Road, 
+    Wadala (E), Mumbai - 400037, INDIA.   
+    T: +91-22-4095 6666
+    E: superjuniorzexpo@gmail.com  |  W: www.peppermint.co.in</pre>
+    </div>
+<div style = "clear:both;"></div>
