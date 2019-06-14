c11550a3fb0a4a133cbdbeb0f396c899885870da
Rahul Nandrajog <2016.rahul.nandrajog@ves.ac.in>
2019-06-14 22:32:34 +0530

    form 10 added

---
+++ exhibitor/exhibitor_optional_form10.php
@@ -0,0 +1,120 @@
+<?php require('../utils/form_logo_details.php'); ?>
+
+    <div class="col-md-12 col-sm-12">
+    <p class="table">
+        <table style="width:100%;">
+            <tr style="background-color:rgb(193, 13, 109);">
+                <th>FORM 10</th>
+                <th>INCOMING ITEMS LABEL
+                    Please ensure each box and/or item has this incoming items label.</th>
+                <th>OPTIONAL</th>
+            </tr>
+        </table>
+    </p>
+    </div>
+    <div class="col-md-12 col-sm-12">
+        <table style="width:90%;">
+            <tr style="background-color:rgb(193, 13, 109);">
+                <th>Receiver Details</th>
+                <th>CHENNAI TRADE CENTER</th>
+            </tr>
+            <tr>
+                <td>Tamilnadu Trade Promotion <br>
+                    Organisation, CTC Complex, <br>
+                    Nandambakkam, Chennai 600089 <br>
+                </td>
+                <td>Contact: Mr. D. Prakash <br>
+                    Mobile: +91 9176313439 / 9176881000 <br>
+                    Tel.: +91 44 22316033 / 65480175 <br>
+                    Email: tntpo@chennaitradecentre.org <br>
+                </td>
+            </tr>
+        </table>
+        <table style="width:90%;">
+            <tr style="background-color:rgb(193, 13, 109);">
+                <th colspan=2>Event Details</th>
+            </tr>
+            <tr>
+                <td>Event Name:
+                </td>
+                <td>SUPER JUNIORZ 2019
+                </td>
+            </tr>
+            <tr>
+                <td>Event Date:
+                </td>
+                <td>24-25th June 
+                </td>
+            </tr>
+            <tr>
+                <td>Event Contact:
+                </td>
+                <td>Ganesh Agawane – Production Head <br>Peppermint Communications Pvt. Ltd.
+                </td>
+            </tr>
+        </table>
+        
+        <table style="width:90%;">
+            <tr style="background-color:rgb(193, 13, 109);">
+                <th colspan=2>Reference:</th>
+            </tr>
+            <tr>
+                <td rowspan=2>Booth Name & Number:
+                </td>
+                <td rowspan=2><input type="text" class="data-input" name="booth_name_number">
+                </td>
+            </tr>
+        </table>
+        <table style="width:90%;">
+            <tr style="background-color:rgb(193, 13, 109);">
+                <th colspan=2>Senders details</th>
+            </tr>
+            <tr>
+                <td>Company name</td>
+                <td><input type="text" class="data-input" name="company_name_final"></td> 
+            </tr>
+            <tr>
+                <td >(Include Name of Company <br> Representative)
+                </td>
+                <td ><input type="text" class="data-input" name="name_of_representive">
+                </td>
+            </tr>
+            <tr>
+                <td >Address: <br> (As per Mailing Address)
+                </td>
+                <td ><input type="text" class="data-input" name="address_final">
+                </td>
+            </tr>
+            <tr>
+                <td>Telephone:
+                </td>
+                <td><input type="text" class="data-input" name="telephone_final">
+                </td>
+            </tr>
+            <tr>
+                <td>Fax:
+                </td>
+                <td><input type="text" class="data-input" name="fax_final">
+                </td>
+            </tr>
+            <tr>
+                <td>Email:
+                </td>
+                <td><input type="text" class="data-input" name="email_final">
+                </td>
+            </tr>
+        </table>
+        <table style="width:90%;">
+            <tr style="background-color:rgb(193, 13, 109);">
+                <th colspan=2>Item Details</th>
+            </tr>
+            <tr>
+                <td rowspan=2>Description of Items <br>i.e. 1 box and 2 banners
+                </td>
+                <td rowspan=2><input type="text" class="data-input" name="booth_name_number">
+                </td>
+            </tr>
+        </table>
+<p><strong>PLEASE NOTE :</strong>Please paste copy of this parcel form on each box / parcel. No parcels / deliveries will be accepted before 20th June 2019, hence making your dispatch arrangements accordingly. Organisor nor Trade Center, Chennai takes responsibility of damages to the goods. Parcels sent to Trade Center, Chennai shall be released on 23rd June 2019 after due verification of receiver.</p>
+
+    </div>
