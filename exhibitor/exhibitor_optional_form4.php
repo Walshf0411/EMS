85ce296acf1af833fe60c199e5e1b91e72d08725
Walsh Fernandes <walshfernades.320@gmail.com>
2019-06-14 14:08:23 +0530

    Added optional form 1

---
+++ exhibitor/exhibitor_optional_form4.php
@@ -0,0 +1,118 @@
+    
+    
+    
+    <p class="table">
+        <table style="width:100%;">
+            <tr style="background-color:rgb(193, 13, 109);">
+                <th>FORM 4</th>
+                <th>ADVERTISING IN FAIR CATALOGUE</th>
+                <th>OPTIONAL</th>
+            </tr>
+            <tr>
+                <td colspan="3">Submission Date - 8th June 2019 </td>
+            </tr>
+        </table>
+    </p>
+
+    <p>
+        <strong>ADVERTISING IN FAIR CATALOGUE</strong>
+        Advertise in the Fair Catalogue and gain maximum advantage of your participation in Super Juniorz.
+        Advertising in the Fair Catalogue is cost effective and will be retained by the trade visitors as a sourcing referencer.
+    </p>
+
+    <p>
+        <strong>Advertising rates: </strong>
+        <div class="row">
+            <div class="col-md-6 col-sm-12">
+                <div class="col-md-12">
+                    <table style="width:100%;">
+                        <tr style="background-color:rgb(193, 13, 109);color:white;">
+                            <th>Position</th>
+                            <th>Rate(Rs.)</th>
+                        </tr>
+                        <tr>
+                            <td>Front Gate</td>
+                            <td>30,000</td>
+                        </tr>
+                        <tr>
+                            <td>Back Cover</td>
+                            <td>25,000</td>
+                        </tr>
+                        <tr>
+                            <td>Inside front cover</td>
+                            <td>22,000</td>
+                        </tr>
+                        <tr>
+                            <td>Inside back cover</td>
+                            <td>20,000 </td>
+                        </tr>
+                        <tr>
+                            <td>Opening Page</td>
+                            <td>18,000 </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+            <div class="col-md-6 col-sm-12">
+                <div class="col-md-11">
+                    <table style="width:100%;">
+                        <tr style="background-color:rgb(193, 13, 109);color:white;">
+                            <th>Position</th>
+                            <th>Rate(Rs.)</th>
+                        </tr>
+                        <tr>
+                            <td>Front Gate</td>
+                            <td> </td>
+                        </tr>
+                        <tr>
+                            <td>First 10 Pages(Right)</td>
+                            <td>25,000</td>
+                        </tr>
+                        <tr>
+                            <td>First 10 Pages(Left)</td>
+                            <td>14,000</td>
+                        </tr>
+                        <tr>
+                            <td>Inside pages(Right)</td>
+                            <td>12,000 </td>
+                        </tr>
+                        <tr>
+                            <td>Inside pages(Left)</td>
+                            <td>10,000 </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </p>
+
+    <pre>
+<strong>Full page size (Techinical Specifications) :</strong>
+• Trim Size: 125mm X 215mm
+• Bleed: 150mm X 210mm
+• Non-bleed: 140mm X 200mm
+• Require Bleed margin of 5mm on all side of the page.
+    </pre>
+
+    <p>
+        <strong>Format :</strong>
+        <p>Files must be provided to us in the following format(s): EPS | CDR | PSD | PDF
+        ------------------------------------------------------------------------------------------------------------------------------------------------------------ 
+        I would wish to place an Advertisement in the Fair Catalogue</p>
+        <label for="Position">Position <input type="text" class="data-input"style="width:70%;"></label>
+        <p>Please find enclosed Cheque / DD of amount <input type="text" class="data-input" style="width:20%;"> drawn in favour of “Peppermint Communications Pvt. Ltd.” and payable in Mumbai.</p>
+    </p>
+
+    <pre>
+<strong>FOR ADVERTISEMENT QUERY:</strong>
+<strong>CONTACT:</strong>Farheen Khan
+            +91 80808 99927
+            farheen.khan@peppermint.co.in
+    </pre>
+
+    <pre>
+<strong>IMPORTANT:</strong>
+1. Please provide ‘open files’ along with the fonts used in the advertisement
+2. Provide us the Advertisement material on CD
+3. All materials and payment must reach us on or before 8th June 2019.
+    </pre>
