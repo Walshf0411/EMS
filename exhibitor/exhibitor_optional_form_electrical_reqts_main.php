0b1c6e096052a0a638de20a17fcade8bfc37ee43
Walsh Fernandes <walshfernades.320@gmail.com>
2019-06-27 12:54:08 +0530

    All form invoice popup done.

--- exhibitor/exhibitor_optional_form_electrical_reqts_main.php
+++ exhibitor/exhibitor_optional_form_electrical_reqts_main.php
@@ -1,9 +1,9 @@
-<ul class="nav nav-pills" id="myTab" role="tablist">
+<ul class="nav nav-tabs" id="myTab" role="tablist">
     <li class="nav-item">
-        <a class="nav-link active" id="electrical-requirements-tab-1" data-toggle="pill" href="#electrical-requirements-1" role="tab" aria-controls="electrical" aria-selected="true">Electrical Requirements 1</a>
+        <a class="nav-link active" id="electrical-requirements-tab-1" data-toggle="tab" href="#electrical-requirements-1" role="tab" aria-controls="electrical" aria-selected="true">Electrical Requirements 1</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link" id="electrical-requirements-tab-2" data-toggle="pill" href="#electrical-requirements-2" role="tab" aria-controls="profile" aria-selected="false">Electrical Requirements 2</a>
+        <a class="nav-link" id="electrical-requirements-tab-2" data-toggle="tab" href="#electrical-requirements-2" role="tab" aria-controls="profile" aria-selected="false">Electrical Requirements 2</a>
     </li>
 </ul>
 <div class="tab-content" id="myTabContent">
